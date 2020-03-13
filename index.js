0636df8d9129a57df6c107e3c147de1649095efa
proudloyaltier <timediamail@gmail.com>
2020-03-13 17:13:28 -0400

    Adds updating and you know alpha
    Hopefully this works

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+window.inactive = false
+window.currentMarkers = [];
+
+function idleTimer() {
+    var t;
+    //window.onload = resetTimer;
+    window.onmousemove = resetTimer; // catches mouse movements
+    window.onmousedown = resetTimer; // catches mouse movements
+    window.onclick = resetTimer;     // catches mouse clicks
+    window.onscroll = resetTimer;    // catches scrolling
+    window.onkeypress = resetTimer;  //catches keyboard actions
+
+    function pauseActivity() {
+        inactive = true
+        confirm("You've been inactive for a while! Click 'Ok' to continue using Infinite Radar", function (confirmed) {
+            if (confirmed) {
+                resetTimer()
+                inactive = false
+            }
+        })
+    }
+
+    function resetTimer() {
+        clearTimeout(t);
+        t = setTimeout(pauseActivity, 900000);  // time is in milliseconds (1000 is 1 second)
+    }
+}
+idleTimer();
+
+
 mapboxgl.accessToken = 'pk.eyJ1IjoiaHltZW5vcHVzIiwiYSI6ImNrN21mbDE1NDBoYTMzbG8waHIzODRnZmQifQ.Sm-p5MctdQjaXASfQWejog';
 var map = new mapboxgl.Map({
     container: 'map',
@@ -70,23 +100,41 @@ var getJSON = function (url, callback) {
     xhr.send();
 };
 
-getJSON('http://infinite-flight-public-api.cloudapp.net/v1/Flights.aspx?apikey=35f43e73-c592-4ed6-8849-0965db7e2df7&sessionid=7e5dcd44-1fb5-49cc-bc2c-a9aab1f6a856',
-    function (err, data) {
-        if (err !== null) {
-            alert('Something went wrong: ' + err);
-        } else {
-            data.forEach(function(aircraft) {
-
-                // create a HTML element for each feature
-                var icon = document.createElement('div');
-                icon.className = 'marker';
-              
-                // make a marker for each feature and add to the map
-                new mapboxgl.Marker(icon)
-                  .setLngLat([aircraft.Longitude, aircraft.Latitude])
-                  .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-                  .setHTML('<h3> Callsign: ' + aircraft.CallSign + '</h3><h3> Display Name: ' + aircraft.DisplayName + '</h3><h3> Speed: ' + Math.round(aircraft.Speed) + '</h3><h3> Altitude: ' + Math.round(aircraft.Altitude) + '</h3><h3> Vertical Speed: ' + Math.round(aircraft.VerticalSpeed) + '</h3><h3> Track: ' + Math.round(aircraft.Track) + '</h3>'))
-                  .addTo(map);
-              })
+function loadAircraft() {
+    if (inactive !== true) {
+        if (currentMarkers !== null) {
+            for (var i = 0; i < (currentMarkers.length - 1); i++) {
+                currentMarkers[i].content.remove();
+            }
         }
-    })
+        getJSON('http://infinite-flight-public-api.cloudapp.net/v1/Flights.aspx?apikey=35f43e73-c592-4ed6-8849-0965db7e2df7&sessionid=7e5dcd44-1fb5-49cc-bc2c-a9aab1f6a856',
+            function (err, data) {
+                if (err !== null) {
+                    alert('Something went wrong: ' + err);
+                } else {
+                    data.forEach(function (aircraft) {
+
+                        // create a HTML element for each feature
+                        var icon = document.createElement('div');
+                        icon.className = 'marker';
+
+                        // make a marker for each feature and add to the map
+                        let newMarker = new mapboxgl.Marker(icon)
+                            .setLngLat([aircraft.Longitude, aircraft.Latitude])
+                            .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+                                .setHTML('<h3> Callsign: ' + aircraft.CallSign + '</h3><h3> Display Name: ' + aircraft.DisplayName + '</h3><h3> Speed: ' + Math.round(aircraft.Speed) + '</h3><h3> Altitude: ' + Math.round(aircraft.Altitude) + '</h3><h3> Vertical Speed: ' + Math.round(aircraft.VerticalSpeed) + '</h3><h3> Track: ' + Math.round(aircraft.Track) + '</h3>'))
+                            .addTo(map);
+
+                        currentMarkers.push(
+                            {
+                                id: aircraft.FlightID,
+                                content: newMarker
+                            })
+                    })
+                }
+            })
+    }
+}
+
+setInterval(loadAircraft, 30000)
+loadAircraft()
