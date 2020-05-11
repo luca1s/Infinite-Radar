3d9ab1d944030bcbc2c1ce56a6dd23314a058123
HymenopusC <34107739+HymenopusC@users.noreply.github.com>
2020-05-11 11:38:45 -0400

    Map tilting and 3d buildings

--- index.js
+++ index.js
@@ -259,8 +259,56 @@ var map = new mapboxgl.Map({
     zoom: '1',
     antialias: true,
 });
-map.dragRotate.disable();
-map.touchZoomRotate.disableRotation();
+
+map.on('load', function () {
+    // Insert the layer beneath any symbol layer.
+    var layers = map.getStyle().layers;
+
+    var labelLayerId;
+    for (var i = 0; i < layers.length; i++) {
+        if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+            labelLayerId = layers[i].id;
+            break;
+        }
+    }
+
+    map.addLayer(
+        {
+            'id': '3d-buildings',
+            'source': 'composite',
+            'source-layer': 'building',
+            'filter': ['==', 'extrude', 'true'],
+            'type': 'fill-extrusion',
+            'minzoom': 15,
+            'paint': {
+                'fill-extrusion-color': '#aaa',
+
+                // use an 'interpolate' expression to add a smooth transition effect to the
+                // buildings as the user zooms in
+                'fill-extrusion-height': [
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    15,
+                    0,
+                    15.05,
+                    ['get', 'height']
+                ],
+                'fill-extrusion-base': [
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    15,
+                    0,
+                    15.05,
+                    ['get', 'min_height']
+                ],
+                'fill-extrusion-opacity': 0.6
+            }
+        },
+        labelLayerId
+    );
+});
 
 document.getElementById('map').style.height = "100%"
 
@@ -418,6 +466,8 @@ function updateAircraft() {
                                 .setLngLat([long, lat])
 
                             currentMarkers[i].content.setRotation(heading - 45);
+                            currentMarkers[i].content.setRotationAlignment("map")
+                            currentMarkers[i].content.setPitchAlignment("map")
                         }
                     }
                     data.forEach(function (aircraft) {
@@ -709,6 +759,8 @@ function loadAircraft() {
                         let newMarker = new mapboxgl.Marker(icon)
                             .setLngLat([aircraft.Longitude, aircraft.Latitude])
                             .addTo(map);
+                        newMarker.setRotationAlignment("map")
+                        newMarker.setPitchAlignment("map")
                         newMarker.setRotation(aircraft.Heading - 45);
                         currentMarkers.push(
                             {
